209f26848f3c57e94d9f1f4d0c0669b67b84059a
Avraj19 <avrajsbw@gmail.com>
2020-05-29 15:59:51 +0100

    5 itit methods and 5 normal methods

--- cat_class.py
+++ cat_class.py
@@ -1,14 +1,26 @@
 class Cat():
 
-    def __init__(self, name='', age='', paws='', fur=''):
+    def __init__(self, name='', age='', fur=''):
         # setting attribute name to instances of Dog class
         self.name = name
         self.age = age
-        self.paws = paws
+        self.paws = '4'
         self.fur = fur
+        self.tail = 'fluffy tail'
+        self.whiskers = 'long whiskers'
+        self.cat_eyes = 'perusing eyes that can tell the future'
 
     def stretch(self):
         return 'yyyyyaaaauuu \n'
 
+    def jumps(self):
+        return 'I always land on my paws'
+
     def eat(self, food=''):
         return 'I\'m hungry' + food
+
+    def sleep(self):
+        return 'zzZZzzZZz ZZzzzZZzz'
+
+    def pur(self):
+        return 'pet me pet'
